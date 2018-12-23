09ce0cf8ca9db8aabfcb87c37a312cd65cf2baf6
TeamProg2018 <teamprog2018@gmail.com>
2018-12-23 08:22:10 +0200

    UAN corrected to UAH

--- src/main/java/com/project20/money/Entity/MyQuotation.java
+++ src/main/java/com/project20/money/Entity/MyQuotation.java
@@ -5,7 +5,7 @@ public class MyQuotation {
     public Float usd_uah=27.3f;
     public Float eur_usd=1.1370f;
     public Float eur_uan=31.0f;
-    public Float uah_usd=27.7f;
+    public Float uah_usd=0.0366f;
     public Float uah_eur=31.65f;
 
 
