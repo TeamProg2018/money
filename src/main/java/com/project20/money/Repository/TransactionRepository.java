541768b9fbb32fde4591a65a087f22eb5d28c2ce
TeamProg2018 <teamprog2018@gmail.com>
2018-12-26 13:36:43 +0200

    Qwerty2018@

--- src/main/java/com/project20/money/Repository/TransactionRepository.java
+++ src/main/java/com/project20/money/Repository/TransactionRepository.java
@@ -3,5 +3,6 @@ package com.project20.money.Repository;
 import com.project20.money.Entity.TransactionsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<TransactionsModel,Long> {
+public interface TransactionRepository extends JpaRepository<TransactionsModel,Long>
+{
 }
