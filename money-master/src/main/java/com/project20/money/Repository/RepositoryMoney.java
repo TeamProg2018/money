97aaf84e619c5fd1185ae4d5d7496ea401fba067
s <stanislavp1001@gmail.com>
2018-12-27 17:15:16 +0200

    css js work

--- money-master/src/main/java/com/project20/money/Repository/RepositoryMoney.java
+++
@@ -1,8 +0,0 @@
-package com.project20.money.Repository;
-
-import com.project20.money.Entity.EntityMoney;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RepositoryMoney extends JpaRepository<EntityMoney,Long> {
-}
--- money-master/src/main/java/com/project20/money/Repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project20.money.Repository;
-
-import com.project20.money.Entity.TransactionsModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<TransactionsModel,Long> {
-}
